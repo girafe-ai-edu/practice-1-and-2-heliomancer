4746c8ff0269198583455b7890021a32607c3cec
Heliomancer <elushoviv@gmail.com>
2024-09-15 22:27:35 +0300

    homework_01

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -3,9 +3,11 @@
 Write a program to calculate the body mass index (BMI) of a person. The user must enter their height and weight, after which you use one of the formulas below to determine the index.
 BMI = weight/height**2 
 """
-weight = input()
-height = input()
+print('Enter weight: (int of float)')
+weight = float(input())
+print('Enter height: (int of float)')
+height = float(input())
 
-
+print(f'Your BMI = {weight/(height**2)}')
 #code here
 
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,13 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+print("Insert a integer to calculate sum of it's digits")
+ii = int(input())
+
+sum = 0
+while ii != 0:
+    sum += ii % 10
+    ii /= 10
+    ii = int(ii)
+
+print(f'Sum of digits euqals {sum}')
